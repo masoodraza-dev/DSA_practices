5eb6ccbb2790f9e757168c022498b2e1c5426138
masoodraza-dev <masoodr508@gmail.com>
2026-04-12 12:56:02 +0500

    Add stack implementation in Stack.cpp
    Implement a stack data structure using linked nodes with push, pop, and display functionalities.

---
+++ Stack.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class stack{
+    private:
+        Node* top;
+
+    public:
+    stack(){
+        top = NULL;
+    }
+
+    void pushStack(int val){
+        Node* newNode = new Node(val);
+        if(top ==NULL){
+            top=newNode;
+            return;
+        }
+        
+        newNode->next = top;
+        top = newNode;
+
+    }
+
+    void popStack(){
+        
+        if(top == NULL){
+            cout<< "The stack is empty: " << endl;
+            return;
+        }
+
+        Node* temp = top;
+        top = top->next;
+        temp->next =NULL;
+        delete temp;
+        cout<< "value poped:\n";
+    }
+
+    void showStack(){
+        if(top==NULL){
+            cout<<"stack is empty:";
+            return;
+        }
+
+        Node* temp = top;
+        while(temp != NULL){
+            cout << "->" << temp->data ;
+            temp=temp->next;
+        }
+
+        
+    }
+};
+
+int main(){
+    stack list;
+
+    list.pushStack(40);
+    list.pushStack(30);
+    list.pushStack(20);
+    list.pushStack(10);
+
+    list.showStack();
+
+    list.popStack();
+
+    list.showStack();
+ return 0;
+}
